ca62a7c87fbfea08adb34e4e6bca33a9b596ba90
COAB1 <carol.o.ag@gmail.com>
2020-07-25 23:45:29 -0300

    create Game class

--- parser.rb
+++ parser.rb
@@ -1,6 +1,26 @@
+Class Game
+    attr_accessor :number, :players, :kills
+
+    def initialize(game_number)
+        @number = game_number
+        @players = []
+        @kills = Hash.new
+    end
+end
+
+
 file = File.read("games.log")
 # .each do | line |
 
 array = file.split("InitGame")
+#puts array.length to check games quantity in the log file
 
-puts array.length
+#first try to group games content
+array.each do | line |
+    $total_kills = 0
+    if line.match?("Kill") #it needs to be a loop 
+        $total_kills += 1
+    end
+    puts $total_kills
+end
+
